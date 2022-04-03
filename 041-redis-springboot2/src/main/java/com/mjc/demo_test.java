dad8360dc9eaa5fbc3a7b5657bde88159fb68e80
majunchao <294136893@qq.com>
2022-04-03 16:19:22 +0800

    second commit

---
+++ 041-redis-springboot2/src/main/java/com/mjc/demo_test.java
@@ -0,0 +1,17 @@
+package com.mjc;
+
+import io.lettuce.core.ScriptOutputType;
+
+/**
+ * ClassName:demo_test
+ * Package:com.mjc
+ * Description: 描述信息
+ *
+ * @date:2022/4/3 16:17
+ * @author:白白白
+ */
+public class demo_test {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
